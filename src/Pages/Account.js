bdb285f2d543b48273dca7276ed8c4e577a56ec9
Limbo395 <144790046+Limbo395@users.noreply.github.com>
2024-05-24 22:33:26 +0300

    Remake auth page

--- src/Pages/Account.js
+++ src/Pages/Account.js
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
 import Header from "../Components/Header";
+import { Button } from "react-bootstrap";
 
 export default class Account extends Component {
+  logOut = () => {
+    window.location.href = '/';
+  };
+
   render() {
     return (
       <>
         <Header />
         <h2>Account</h2>
+        <Button onClick={this.logOut}>LogOut</Button>
       </>
     );
   }
