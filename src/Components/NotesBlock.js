439b38584daeff91e051a6a9782934e6153d7fdd
Limbo395 <144790046+Limbo395@users.noreply.github.com>
2024-05-30 22:40:01 +0300

    Add visual to Notes page

---
+++ src/Components/NotesBlock.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import avatar from "../Pictures/standart-avatar.png";
+import deletePng from "../Pictures/trashPng.png";
+import notePng from "../Pictures/note-png.png";
+import "./NotesBlock.css";
+
+export default function FriendBlock({ data, link }) {
+  return (
+    <>
+      <div className="div-border">
+        <img
+          style={{ margin: "10px" }}
+          src={notePng}
+          alt="Your Image"
+          className="image"
+        />
+        <strong style={{ fontSize: "20px" }}>{data}</strong>
+
+        <div style={{ marginLeft: "auto" }}>
+          <h5 style={{ margin: "20px" }}>
+            <strong>Edit</strong>
+          </h5>
+        </div>
+      </div>
+    </>
+  );
+}
